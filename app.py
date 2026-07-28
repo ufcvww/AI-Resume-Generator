c512d6e05d2a551d2a0489ee7613e18246a6587f
ufcvww <raina.ujjwal.69@gmail.com>
2026-07-28 12:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
 GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
 
+if not (GOOGLE_API_KEY) and not (GROQ_API_KEY) and not (TAVILY_API_KEY):
+    st.sidebar.warning("PASS API KEYS")
+    st.stop()
+else:
+    st.sucess("API KEYS LOADED")
+
+
 #========= MODEL BUILDING ===============
 
 model = ChatGoogleGenerativeAI(
@@ -92,6 +99,29 @@ def resume_maker_prompt():
   return prompt
     
 resume_maker_prompt()
+
+#===========================UPLOAD IMAGE============================================
+uploaded_file = st.sidebar.file_uploader( 
+    "Choose an image file",
+    type=["jpg", "jpeg", "png", "webp"]
+)
+if uploaded file is not None:
+    try:
+        image = Image.open(uploaded_file)
+    
+        st.sidebar.image(image, caption="Uploaded Image", use_container_width=True)
+        
+        if image.mode in ("RGBA", "P"):
+           image = image.convert("RGB") 
+        base_name = os.path.splitext(uploaded_file.name) [0] 
+        save_path = f"{base_name}.jpg"
+        
+        image.save(save_path, "JPEG") 
+        st.sidebar.success (f" Image successfully saved as `{save_path}!")
+        
+    except Exception as e:
+        st.error(f"🎉Error processing image: {e}")
+        
 # ========= GENERATE RESUME ==========
 
 prompt = """You are a helpful AI assistant
@@ -102,6 +132,16 @@ User will upload data and return HTML format resume
 Always use different styling"""
 
 final_prompt = prompt + resume_maker_prompt()
+
+user_info = st.text_input("Enter your information")
+
+user_details = f"""user details: given below:
+Resume info: {user_info}
+Photo: {uploaded_file }
+photo present in current directory with name as 
+uploaded_file, and once resume generated give 
+download button in same html code.
+Default if not given: Give Python Developer Resume"""
 
 user_details = """user details : given below :
 My name is Ujjwal Raina currently studying in 2nd year ip university
